827d326112afd6805c7d1042f2b13fa5321bf7bb
XuYi-fei <857998989@qq.com>
2021-10-20 18:23:57 +0800

    feat: Add decision tree way to predict the results

---
+++ config/tree_decision_config.py
@@ -0,0 +1,37 @@
+import argparse
+
+
+class TreeDecisionConfig:
+    def __init__(self, configs):
+        self.criterion = configs.criterion
+        self.splitter = configs.splitter
+        self.max_depth = configs.max_depth
+        self.test = configs.test
+        self.model = configs.model
+        # self.min_samples_split = 2,
+        # self.min_samples_leaf = 1,
+        # self.min_weight_fraction_leaf = 0.,
+        # self.max_features = None,
+        # self.random_state = None,
+        # self.max_leaf_nodes = None,
+        # self.min_impurity_decrease = 0.,
+        # self.min_impurity_split = None,
+        # self.class_weight = None,
+        # self.presort = 'deprecated',
+        # self.ccp_alpha = 0
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--max-depth", help="max-depth of the decision tree", type=int, default=5)
+    parser.add_argument("--criterion", "-c", help="judge criterion of the tree", type=str, choices=['gini', 'entropy'],
+                        default='gini')
+    parser.add_argument("--splitter", "-s", help="splitter strategy of the tree", type=str, choices=['best', 'random'],
+                        default='best')
+    parser.add_argument("--test", "-t", help="train or test", type=bool, default=True)
+    parser.add_argument("--model", help="path to saved model", type=str, default='treeCheckpoints/10-20-14-18.pkl')
+    return parser.parse_args()
+
+
+arguments = parse_args()
+config = TreeDecisionConfig(arguments)
---
+++ decision_tree.py
@@ -0,0 +1,60 @@
+from sklearn import tree
+import pandas as pd
+import numpy as np
+import graphviz
+import time
+import pickle
+from config.tree_decision_config import config
+
+
+class TreeTrainSeedDataset:
+
+    def __init__(self, annotations_file):
+        self.data: pd.DataFrame = pd.read_csv(annotations_file)
+        self.data: pd.DataFrame = self.data[self.data['label'].notna()]
+        self.Y = self.data['label']
+        self.X = self.data.drop(['id', 'label'], axis=1).fillna(value=-1)
+        self.X = np.array(self.X).tolist()
+        self.Y = list(np.array(self.Y).astype(np.int64))
+
+    def __len__(self):
+        return len(self.data)
+
+
+class TreeTestSeedDataset:
+    def __init__(self, label_file):
+        self.data: pd.DataFrame = pd.read_csv(label_file)
+        self.X = self.data.drop(['id'], axis=1).fillna(value=-1)
+        self.X = np.array(self.X).tolist()
+
+
+def train():
+    model = tree.DecisionTreeqClassifier(max_depth=config.max_depth, criterion=config.criterion,
+                                        splitter=config.splitter)
+    dataset = TreeTrainSeedDataset("./data/v1/train.csv")
+    model = model.fit(dataset.X, dataset.Y)
+
+    # draw the tree, output file is named by current time
+    dot_data = tree.export_graphviz(model, out_file=None)
+    graph = graphviz.Source(dot_data)
+    current_time = str(time.strftime("%m-%d-%H-%M", time.localtime()))
+    graph.render(current_time)
+    with open('treeCheckpoints\\' + current_time + '.pkl', 'wb') as output:
+        pickle.dump(model, output)
+
+
+def test():
+    with open(config.model, 'rb') as input:
+        model = pickle.load(input)
+    dataset = TreeTestSeedDataset("./data/v1/test_a.csv")
+    result = model.predict(dataset.X)
+    result = map(lambda x : str(x), result)
+    with open("result.txt", "w") as f:
+        f.write("\n".join(result))
+
+
+if __name__ == "__main__":
+    if config.test:
+        test()
+    else:
+        train()
