f246de3c3fa54972f00258bce44f3d8af39f3d6f
Sharp-rookie <1842604700@qq.com>
2021-10-30 00:59:05 +0800

    :bug:

--- ML/SVM.py
+++ ML/SVM.py
@@ -10,7 +10,7 @@ import argparse
 
 
 class SVM_SVC:
-    def __init__(self, clf, kernel, C, trainfile="../data/ML/train.csv", validfile="../data/ML/valid.csv", testfile="../data/ML/test.csv"):
+    def __init__(self, clf, kernel, C, degree, trainfile="../data/ML/train.csv", validfile="../data/ML/valid.csv", testfile="../data/ML/test.csv"):
         super(SVM_SVC, self).__init__()
 
         train: pd.DataFrame = pd.read_csv(trainfile)
@@ -28,19 +28,20 @@ class SVM_SVC:
         if(clf == "SVC"):
             if(kernel == "rbf"):
                 self.classifier = Pipeline([
-                    ('pca',PCA()), 
-                    ("scaler", StandardScaler()), 
+                    # ('pca',PCA()),
+                    ("scaler", StandardScaler()),
                     ("svm_clf", SVC(kernel="rbf", gamma='auto', C=C))])
             if(kernel == "poly"):
                 self.classifier = Pipeline([
-                    ("scaler", StandardScaler()), 
-                    ("svm_clf", SVC(kernel="poly", degree=3, coef0=1, C=C))])
+                    # ('pca',PCA()),
+                    ("scaler", StandardScaler()),
+                    ("svm_clf", SVC(kernel="poly", degree=degree, coef0=1, C=C))])
 
         if(clf == "LinearSVC"):
             self.classifier = Pipeline([
-                    ("poly_featutres", PolynomialFeatures(degree=3)),
-                    ("scaler", StandardScaler()),  # 特征标准化
-                    ("svm_clf", LinearSVC(C=10, loss="hinge", random_state=42))])
+                ("poly_featutres", PolynomialFeatures(degree=degree)),
+                ("scaler", StandardScaler()),  # 特征标准化
+                ("svm_clf", LinearSVC(C=10, loss="hinge", random_state=42))])
 
     def fit(self):
         self.classifier.fit(self.train_x, self.train_y)
@@ -48,20 +49,19 @@ class SVM_SVC:
     def P(self):
         index_ = self.classifier.predict(self.valid_x) == 1
         TP = (self.valid_y[index_] == 1).sum()
-        return TP / index_.sum()
+        return round(TP / index_.sum(), 4)
 
     def R(self):
         index_ = self.valid_y == 1
         TP = (self.classifier.predict(self.valid_x)[index_] == 1).sum()
 
-        return TP / index_.sum()
-    
+        return round(TP / index_.sum(), 4)
+
     def Fscore(self):
         P = self.P()
         R = self.R()
 
-        return 5 * P * R / (3 * P + 2 * R)
-
+        return round(5 * P * R / (3 * P + 2 * R), 4)
 
     def predict(self):
         return self.classifier.predict(self.test).astype(float)
@@ -77,6 +77,8 @@ def parse_args():
                         default="rbf")
     parser.add_argument('--C', type=float,
                         default=1)
+    parser.add_argument('--degree', type=int,
+                        default=3)
 
     return parser.parse_args()
 
@@ -85,12 +87,13 @@ if __name__ == "__main__":
 
     args = parse_args()
 
-    svm = SVM_SVC(args.clf, args.kernel, args.C)
+    svm = SVM_SVC(args.clf, args.kernel, args.C, args.degree)
     svm.fit()
-    print(f"valid:\n\tPrecision: {svm.P()}\tRecall: {svm.R()}\tFscore: {svm.Fscore()}")
+    print(
+        f"valid:\tPrecision: {svm.P()}\tRecall: {svm.R()}\tFscore: {svm.Fscore()}")
     result = svm.predict().astype(int)
     # print(result)
-    
+
     fp = open("output.txt", "w")
     for i in range(result.shape[0]):
         fp.write(result[i].astype(str))
